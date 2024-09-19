1f27df14b498f79f04ba91e57eadb0678fe4c052
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2024-09-19 10:20:50 +0200

    initial commit

---
+++ Source/ProjectSwordfish/ProjectSwordfish.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectSwordfish.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectSwordfish, "ProjectSwordfish" );
