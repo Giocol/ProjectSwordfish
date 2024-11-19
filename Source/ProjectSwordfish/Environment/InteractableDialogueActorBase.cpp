c06687b6a8a378f80c1abc0ec21c1a12a48b38de
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2024-11-19 12:17:42 +0100

    tech: some progress on ink

---
+++ Source/ProjectSwordfish/Environment/InteractableDialogueActorBase.cpp
@@ -0,0 +1,19 @@
+﻿#include "InteractableDialogueActorBase.h"
+
+
+AInteractableDialogueActorBase::AInteractableDialogueActorBase() {
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AInteractableDialogueActorBase::BeginPlay() {
+	Super::BeginPlay();
+}
+
+void AInteractableDialogueActorBase::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
+
+void AInteractableDialogueActorBase::Interact(AMainCharacter* InteractionInstigator) {
+	IInteractableInterface::Interact(InteractionInstigator);
+}
+
--- StorySource/HelloInk.ink
+++ StorySource/HelloInk.ink
@@ -1,4 +1,7 @@
-Once upon a time...
+Fishing is pretty cool huh?
+
+Scary? Nah what are you talking about?
+
 
  * There were two choices.
  * There were four lines of content but modified
