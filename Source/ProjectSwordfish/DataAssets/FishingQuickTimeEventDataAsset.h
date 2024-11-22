1c9f389f7db9a0ee675781e135276abfb9f2345a
Jo Colomban <34074840+Giocol@users.noreply.github.com>
2024-11-22 14:21:19 +0100

    tech: add more options to QTE data asset

--- Source/ProjectSwordfish/DataAssets/FishingQuickTimeEventDataAsset.h
+++ Source/ProjectSwordfish/DataAssets/FishingQuickTimeEventDataAsset.h
@@ -4,6 +4,8 @@
 #include "Engine/DataAsset.h"
 #include "FishingQuickTimeEventDataAsset.generated.h"
 
+
+
 UCLASS()
 class PROJECTSWORDFISH_API UFishingQuickTimeEventDataAsset : public UPrimaryDataAsset
 {
@@ -22,21 +24,31 @@ public:
 	UPROPERTY(EditDefaultsOnly, Category = "General", meta = (EditCondition="bDoesRepeat", EditConditionHides))
 		bool bIsRepeatCooldownRandomized = false;
 
+	UPROPERTY(EditDefaultsOnly, Category = "General", meta = (EditCondition="bDoesRepeat", EditConditionHides))
+		bool bIsNumberOfRepetitionsRandomized = false;
+
 protected:
 	UPROPERTY(EditDefaultsOnly, Category = "Thresholds", meta = (ClampMin = "0", UIMin = "0", EditCondition="bIsTimeToCompleteRandomized", EditConditionHides))
 		float TimeToCompleteLowerThreshold = 0;
 	
 	UPROPERTY(EditDefaultsOnly, Category = "Thresholds", meta = (ClampMin = "0", UIMin = "0"))
 		float TimeToComplete = 0;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Thresholds | Repetition", meta = (ClampMin = "0", UIMin = "0", EditCondition="bDoesRepeat && bIsNumberOfRepetitionsRandomized", EditConditionHides))
+		int NumberOfRepetitionsLowerThreshold = 1;
 	
-	UPROPERTY(EditDefaultsOnly, Category = "Thresholds", meta = (ClampMin = "0", UIMin = "0", EditCondition="bDoesRepeat && bIsRepeatCooldownRandomized", EditConditionHides))
+	UPROPERTY(EditDefaultsOnly, Category = "Thresholds | Repetition", meta = (ClampMin = "0", UIMin = "0", EditCondition="bDoesRepeat", EditConditionHides))
+		int NumberOfRepetitions = 1;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Thresholds | Repetition", meta = (ClampMin = "0", UIMin = "0", EditCondition="bDoesRepeat && bIsRepeatCooldownRandomized", EditConditionHides))
 		float RepeatCooldownLowerThreshold = 0;
 	
-	UPROPERTY(EditDefaultsOnly, Category = "Thresholds", meta = (ClampMin = "0", UIMin = "0", EditCondition="bDoesRepeat", EditConditionHides))
+	UPROPERTY(EditDefaultsOnly, Category = "Thresholds | Repetition", meta = (ClampMin = "0", UIMin = "0", EditCondition="bDoesRepeat", EditConditionHides))
 		float RepeatCooldown = 0;
 
 public:
 	float GetTimeToComplete() const { return bIsTimeToCompleteRandomized ? TimeToComplete : FMath::RandRange(TimeToCompleteLowerThreshold, TimeToComplete); }
+	float GetNumberOfRepetitions() const { return  bIsNumberOfRepetitionsRandomized ? NumberOfRepetitions : FMath::RandRange(NumberOfRepetitionsLowerThreshold, NumberOfRepetitions); }
 	float GetRepeatCooldown() const { return bIsRepeatCooldownRandomized ? RepeatCooldownLowerThreshold : FMath::RandRange(RepeatCooldownLowerThreshold, RepeatCooldown); }
 	
 #if WITH_EDITOR // Just for editor stuff
@@ -49,7 +61,8 @@ inline void UFishingQuickTimeEventDataAsset::PostEditChangeProperty(FPropertyCha
 {
 	Super::PostEditChangeProperty(PropertyChangedEvent);   
 
-	TimeToCompleteLowerThreshold = FMath::Clamp(TimeToCompleteLowerThreshold, 0, TimeToComplete);
-	RepeatCooldownLowerThreshold = FMath::Clamp(RepeatCooldownLowerThreshold, 0, RepeatCooldown);
+	TimeToCompleteLowerThreshold = FMath::Clamp(TimeToCompleteLowerThreshold, 0.f, TimeToComplete);
+	RepeatCooldownLowerThreshold = FMath::Clamp(RepeatCooldownLowerThreshold, 0.f, RepeatCooldown);
+	NumberOfRepetitionsLowerThreshold = FMath::Clamp(NumberOfRepetitionsLowerThreshold, 0, NumberOfRepetitions);
 }
 #endif
