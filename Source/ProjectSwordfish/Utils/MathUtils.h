59bfc5c8701d935558b46cd918ee2ac5aa075cab
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2024-12-17 10:37:11 +0100

    chore: update MathUtils with unused functions

--- Source/ProjectSwordfish/Utils/MathUtils.h
+++ Source/ProjectSwordfish/Utils/MathUtils.h
@@ -1,15 +1,34 @@
 ﻿#pragma once
 
 namespace MathUtils {
-	static float DegAngleLerp(float Start, float Target, float Alpha) {
-		float Delta = Target - Start;
+	static float AngleLerp(float Current, float Target, float Alpha, float HalfCircleAngle) {
+		float Delta = Target - Current;
 
-		if(Delta > 180) {
-			Delta -= 360;
-		} else if(Delta < -180) {
-			Delta += 360;
+		if(Delta > HalfCircleAngle) {
+			Delta -= HalfCircleAngle * 2;
+		} else if(Delta < -HalfCircleAngle) {
+			Delta += HalfCircleAngle * 2;
 		}
 
-		return Start + Delta * Alpha;
-	}	 
+		return Current + Delta * Alpha;
+	}
+	
+	static float DegAngleLerp(float Current, float Target, float Alpha) {
+		return AngleLerp(Current, Target, Alpha, 180);
+	}
+	
+	static float RadAngleLerp(float Current, float Target, float Alpha) {
+		return AngleLerp(Current, Target, Alpha, PI);
+	}
+	
+	static FVector2d PolarLerp(FVector2d Current, FVector2d Target, float Alpha) {
+		float LerpedY = RadAngleLerp(Current.Y, Target.Y, Alpha);
+		float LerpedX = RadAngleLerp(Current.X, Target.X, Alpha);
+	
+		return FVector2D(LerpedX, LerpedY);
+	}
+
+	static float GetAngleBetweenVectorsRads(const FVector& Current, const FVector& Target) {
+		return acos(FVector::DotProduct(Current, Target));
+	}
 };
